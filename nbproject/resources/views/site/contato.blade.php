042f620a4ca712a4c47e833c0984c40c0f51c20a
marcelo-alves <mamdria@gmail.com>
2021-07-19 19:57:28 -0300

    principal

---
+++ nbproject/resources/views/site/contato.blade.php
@@ -0,0 +1,96 @@
+@extends('site.template.template')
+
+@section('titulo')
+CONTATO
+@stop
+@section('conteudo')
+    <div class="container">
+        <section class="jumbotron text-center">
+            <div class="container">
+                <h1 class="jumbotron-heading">Contato </h1>                
+            </div>
+        </section>
+        <div id="resp" name="resp" role="alert"> </div>
+        <form action="{{route('enviaremail')}}"  method="get">
+            
+            <div class="form-group">
+                <label for="txtnome">Nome</label>
+                <input type="text" class="form-control" id="txtnome" name="txtnome" placeholder="Digite seu nome">
+            </div>
+            <div class="form-group">
+                <label for="txtemail">Endereço de email</label>
+                <input type="email" class="form-control" id="txtemail" name="txtemail" placeholder="Digite seu email">
+            </div>
+
+            <div class="form-group">
+                <label for="txtobservacao">Observação</label>
+                <textarea id="txtobservacao" name="txtobservacao" class="form-control" rows="3" placeholder="Observação"></textarea>
+            </div>
+            <div class="form-group">
+                <input type="button" name="btnenviar" id="btnenviar" onclick="busca()" value="Enviar" >
+            </div>
+        </form>
+
+    </div>
+    <script>
+        var busca = function(){      
+                document.getElementById('resp').className ="alert alert-secondary";
+                document.getElementById('resp').innerHTML='<h1>Processando .</h1>';
+
+                tnome = document.querySelector("#txtnome").value;
+                temail = document.querySelector("#txtemail").value;
+                tobservacao = document.querySelector("#txtobservacao").value;
+
+                if(tnome =="" || temail =="" ||tobservacao ==""){
+                    document.getElementById('resp').className ="alert alert-danger";
+                    document.getElementById('resp').innerHTML='<h3>Por favor <br> Preencha todos os campos .</h3>';
+                    return false;
+                }
+
+                var token ='{{csrf_token()}}'
+                const dados={
+                        nome:tnome,
+                        email:temail,
+                        observacao:tobservacao
+                }
+                
+                document.getElementById('resp').className ="alert alert-secondary";
+                document.getElementById('resp').innerHTML='<h1>Processando ..</h1>';
+
+
+                const retorno = (resultado) => {
+                    document.getElementById('resp').className ="alert alert-secondary";
+                    document.getElementById('resp').innerHTML='<h1>Processando ..</h1>';
+                    
+                    if(resultado =='sucesso'){
+                        document.getElementById('resp').className ="alert alert-success";
+                        document.getElementById('resp').innerHTML='<h1>Contato Enviado com sucesso.</h1>';
+                        document.querySelector("#txtnome").value='';
+                        document.querySelector("#txtemail").value='';
+                        document.querySelector("#txtobservacao").value='';
+                        document.getElementById("btnenviar").disabled = "true";
+                    }
+                    else{
+                        document.getElementById('resp').className ="alert alert-danger";
+                        document.getElementById('resp').innerHTML='<h1>Erro ao enviar o contato.</h1>';
+                    }    
+                }
+
+                fetch("{{route('enviaremail')}}/",{
+                    method:'POST',
+                    mode:'cors',
+                    headers:{
+                        'X-CSRF-TOKEN':token,
+                        'Content-Type':'application/json'
+                    },
+                    body:JSON.stringify(dados)           
+                })
+                .then(resposta => {  resposta.json()
+                    .then(ddado => {retorno(ddado.resposta)})                                
+                })                     
+        }
+    </script>
+
+
+
+@endsection
---
+++ nbproject/routes/web.php
@@ -0,0 +1,36 @@
+<?php
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PrincipalController;
+use App\Http\Controllers\QuemController;
+use App\Http\Controllers\ContatoController;
+use App\Http\Controllers\NoticiaController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AdminPrincipalController;
+use App\Http\Controllers\AdminContatoController;
+use App\Http\Controllers\AdminNotciasController;
+use App\Http\Controllers\AdminQuemController;
+use GuzzleHttp\Psr7\Request;
+
+/** Route::get('/', function () {    return view('welcome'); }); **/
+/*Route::get('/',function () { 
+    return view('principal');
+});*/
+
+Route::get('/',[PrincipalController::class,'index'])->name('site.home');
+Route::get('/home',[PrincipalController::class,'index'])->name('site.home');
+Route::get('/busca',[PrincipalController::class,'busca'])->name('site.busca');
+Route::get('/quem',[QuemController::class,'index'])->name('site.quem');
+Route::get('/contato',[ContatoController::class,'index'])->name('site.contato');
+Route::post('/enviaremail',[ContatoController::class,'enviaremail'])->name('enviaremail');
+Route::get('/noticia',[NoticiaController::class,'index'])->name('site.noticia');
+Route::get('/painel',[HomeController::class,'index'])->name('admin.login');
+Route::get('/painel/principal', [AdminPrincipalController::class,'index'])->name('admin.principal');
+Route::get('/painel/noticias', [AdminNotciasController::class,'index'])->name('admin.noticias');
+Route::get('/painel/noticias/editor', [AdminNotciasController::class,'editor'])->name('admin.adminnoticiaseditor');
+Route::get('/painel/quem', [AdminQuemController::class,'index'])->name('admin.quem');
+Route::get('/painel/quem/editor', [AdminQuemController::class,'editor'])->name('admin.quemeditor');
+Route::post('/painel/quem/editar', [AdminQuemController::class,'editar'])->name('admin.quemeditar');
+Route::get('/painel/contato', [AdminContatoController::class,'index'])->name('admin.contato');
+/*Route::post('/enviaremail/',function () { 
+       return dd($_REQUEST);
+     })->name('enviaremail'); */
